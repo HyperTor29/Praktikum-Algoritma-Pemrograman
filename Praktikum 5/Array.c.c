fd9fbd301a639446baec5cf9a20196a3e3edae39
HyperTor29 <100137010+HyperTor29@users.noreply.github.com>
2022-03-29 22:27:16 +0700

    Add files via upload

---
+++ Praktikum 5/Array.c.c
@@ -0,0 +1,31 @@
+//Nama File : NBArray.c
+//Deskripsi : Menghitung dan menampilkan besarnya
+//penjumlahan sub array/tabel T.
+
+
+#include <stdio.h>
+
+int main(){
+    // Kamus
+    int k, l, m, sum;
+    // Algoritma
+    printf("Masukkan jumlah elemen: ");
+    scanf("%d", &k);
+    int T[k];
+    sum = 0;
+
+    for (l = 0; l < k; l++) scanf("%d", &T[l]);
+
+    for (l = 0; l < k; l++) {
+        printf("(");
+        for (m = l; m < k; m++) {
+            sum += T[m];
+            if (m != k-1) printf("%d + ", T[m]);
+            else printf("%d", T[m]);
+        }
+        if (l != k-1) printf(") + ");
+        else printf(")");
+    }
+    printf("\nJumlah dari array dan subarraynya adalah: %d", sum);
+    return 0;
+}
---
+++ Praktikum 5/SelisihTinggi.c.c
@@ -0,0 +1,28 @@
+//Nama File : SelisihKetinggian.c
+//Deskripsi : Menghitung dan menampilkan besarnya
+// selisih ketinggian terbesar puncak bukut dengan lembah bukit atau sebaiknya
+
+
+
+#include <stdio.h>
+
+int main(){
+    // Kamus
+    int k, l, max, min;
+    // Algoritma
+    printf("Masukkan jumlah data: ");
+    scanf("%d", &k);
+    int T[k];
+
+    for (l = 0; l < k; l++) scanf("%d", &T[l]);
+
+    max = T[0];
+    min = T[0];
+
+    for (l = 0; l < k; l++) if (T[l] > max) max = T[l];
+
+    for (l = 0; l < k; l++) if (T[l] < min) min = T[l];
+
+    printf("Maksimum: %d\nMinimum: %d\nSelisih ketinggiannya adalah: %d", max, min, max-min);
+    return 0;
+}
